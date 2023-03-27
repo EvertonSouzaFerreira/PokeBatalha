173e37e02d0162cabc0ac73de5d5162d05034c88
EvertonSouzaFerreira <everton.souzaferreira@outlook.com>
2023-03-27 22:02:26 +0200

    correcaoTela

--- src/Components/AparelhoPokedex/AparelhoPokedex.js
+++ src/Components/AparelhoPokedex/AparelhoPokedex.js
@@ -1,65 +1,56 @@
-import React, { useContext, useState } from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import PokedexPage from '../../Pages/PokedexPage/PokedexPage'
-import PokemonDetalhes from '../../Pages/PokemonDetailPage/PokemonDetalhes'
-import PokemonCard from '../PokemonCard/PokemonCard'
+import React, { useContext, useState } from "react";
 
-import CapaPokedex from '../CapaPokedex/CapaPokedex'
-import TelaDeBatalha from '../TelaBatalha/TelaDeBatalha'
-import TelaJogo from '../TelaJogo/TelaJogo'
-import './style2.css'
+import CapaPokedex from "../CapaPokedex/CapaPokedex";
+import TelaDeBatalha from "../TelaBatalha/TelaDeBatalha";
+import TelaJogo from "../TelaJogo/TelaJogo";
+import "./style2.css";
 
-import { PokedexCtx } from '../../PokedexContexto'
+import { PokedexCtx } from "../../PokedexContexto";
 
 function AparelhoPokedex() {
-  const {flip, setFlip} = useContext(PokedexCtx)
+  const { flip, setFlip } = useContext(PokedexCtx);
 
   const [boxRotation, setBoxRotation] = useState({ x: 0, y: 0 });
 
   const handleMouseMove = (e) => {
-    if (flip) return
+    if (flip) return;
     const xAxis = (window.innerWidth / 2 - e.pageX) / 10;
     const yAxis = (window.innerHeight / 2 - e.pageY) / 15;
     setBoxRotation({ x: xAxis, y: yAxis });
     console.log(boxRotation);
-  }
+  };
 
   const { x, y } = boxRotation;
-  const styles = {transform: `rotateY(${x}deg) rotateX(${y}deg)`}
-
+  const styles = { transform: `rotateY(${x}deg) rotateX(${y}deg)` };
 
   return (
-  <div className='containerBox' onMouseMove={handleMouseMove}>
-      <div className="box" style={styles}>
-      <div className="box__face box__face--back"></div>
-      <div className="box__face box__face--front">
+    <div
+      className={flip ? `containerBox move-box` : `containerBox`}
+      onMouseMove={handleMouseMove}
+    >
+      <div className='box' style={styles}>
+        <div className='box__face box__face--back'></div>
+        <div className='box__face box__face--front'>
+          <div className='container'>
+            <div className='blue-btn'></div>
 
+            <div className='container-minilight'>
+              <div className='red-light'></div>
+              <div className='yellow-light'></div>
+              <div className='green-light'></div>
+            </div>
 
-<div className="container">
-
-    <div className="blue-btn"></div>
-    
-      <div className="container-minilight">
-        <div className="red-light"></div>
-        <div className="yellow-light"></div>
-        <div className="green-light"></div>
+            <TelaJogo />
+            <CapaPokedex />
+          </div>
+        </div>
+        <div className='box__face box__face--left'></div>
+        <div className='box__face box__face--right'></div>
+        <div className='box__face box__face--top'></div>
+        <div className='box__face box__face--bottom'></div>
       </div>
-      
-      <TelaJogo/>
-      <CapaPokedex/>
-        
     </div>
-
-
-      </div>
-      <div className="box__face box__face--left"></div>
-      <div className="box__face box__face--right"></div>
-      <div className="box__face box__face--top"></div>
-      <div className="box__face box__face--bottom"></div>
-    </div>
-  </div>  
-    
-  )
+  );
 }
 
-export default AparelhoPokedex
+export default AparelhoPokedex;
